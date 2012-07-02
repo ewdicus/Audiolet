650c8bafc2767e418e25699ec86355cfb5a2c9a7
Elliott Dicus <ewdicus@gmail.com>
2012-07-02 11:20:05 -0700

    Choosing number of channels from output instead of device.

--- src/audiolet/Audiolet.js
+++ src/audiolet/Audiolet.js
@@ -566,16 +566,22 @@ AudioletNode.prototype.tick = function() {
     this.generate();
     
     //Visualization Buffer
+    //This is our buffer size - this.audiolet.device.bufferSize;
     if(!this.visBuffer){
-        this.visBuffer = new Float32Array(4096);
+        this.visBuffer = new AudioletBuffer(this.audiolet.device.numberOfChannels, this.audiolet.device.bufferSize);
         this.visIndex = 0;
     }
-    
-    if(this.visIndex < 4096){
-        this.visBuffer[this.visIndex] = this.outputs[0].samples[0];
-        this.visIndex++;
-    }else{
-        this.visIndex = 0;
+    // this.audiolet.device.numberOfChannels can be different than this.outputs[0].numberOfChannels.
+    // We're just going to use the first outputs number for now
+    for(var i = 0; this.outputs[0].numberOfChannels; i++){
+        var bufferChannel = this.visBuffer.getChannelData(i);
+        if(this.visIndex < this.audiolet.device.bufferSize){
+            //We're only using the first output here.
+            this.bufferChannel[this.visIndex] = this.outputs[0].samples[i];
+            this.visIndex++;
+        }else{
+            this.visIndex = 0;
+        }
     }
 };
 
