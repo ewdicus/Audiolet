d6a108033dc322bd1c07e24d6e531bd5711e35be
Elliott Dicus <ewdicus@gmail.com>
2012-07-02 12:40:31 -0700

    Some better error checking; Picked a buffer length to keep everything even across browsers

--- src/core/AudioletNode.js
+++ src/core/AudioletNode.js
@@ -107,21 +107,27 @@ AudioletNode.prototype.tick = function() {
     this.generate();
     
     //Visualization Buffer
-    //This is our buffer size - this.audiolet.device.bufferSize;
     if(!this.visBuffer){
-        this.visBuffer = new AudioletBuffer(this.audiolet.device.numberOfChannels, this.audiolet.device.bufferSize);
+        var bufferSize = 4096; //Want to keep it the same across browsers. This is what chrome was choosing.
+        var numberOfChannels = 2;
+        if(this.outputs && this.outputs[0]){
+            numberOfChannels = this.outputs[0].numberOfChannels;
+        }
+        this.visBuffer = new AudioletBuffer(numberOfChannels, bufferSize);
         this.visIndex = 0;
     }
     // this.audiolet.device.numberOfChannels can be different than this.outputs[0].numberOfChannels.
     // We're just going to use the first outputs number for now
-    for(var i = 0; this.outputs[0].numberOfChannels; i++){
-        var bufferChannel = this.visBuffer.getChannelData(i);
-        if(this.visIndex < this.audiolet.device.bufferSize){
-            //We're only using the first output here.
-            this.bufferChannel[this.visIndex] = this.outputs[0].samples[i];
-            this.visIndex++;
-        }else{
-            this.visIndex = 0;
+    if(this.outputs && this.outputs[0]){
+        for(var i = 0; i < this.outputs[0].numberOfChannels; i++){
+            var bufferChannel = this.visBuffer.getChannelData(i);
+            if(this.visIndex < this.visBuffer.length){
+                //We're only using the first output here.
+                bufferChannel[this.visIndex] = this.outputs[0].samples[i];
+                this.visIndex++;
+            }else{
+                this.visIndex = 0;
+            }
         }
     }
 };
